a5d938a9c8356a74718374289c5ef6c6d519540f
SuspiciousLight <117872667+SuspiciousLight@users.noreply.github.com>
2026-06-07 01:11:27 +0300

    Stabilize frontend color scheme

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { ConfigProvider } from '@vkontakte/vkui';
+import { ColorSchemeProvider, ConfigProvider } from '@vkontakte/vkui';
 import '@vkontakte/vkui/dist/vkui.css';
 import './styles/global.css';
 import { App } from './app/App';
@@ -8,7 +8,9 @@ import { App } from './app/App';
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ConfigProvider>
-      <App />
+      <ColorSchemeProvider value="dark">
+        <App />
+      </ColorSchemeProvider>
     </ConfigProvider>
   </React.StrictMode>,
 );
